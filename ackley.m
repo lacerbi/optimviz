28ba689acfade3e608c9609a4ce8b4d0e2815902
Luigi Acerbi <luigi.acerbi@nyu.edu>
2016-05-23 10:39:41 -0400

    ackley

--- ackley.m
+++ ackley.m
@@ -1,11 +1,12 @@
-function varargout = ackley( x )
+function varargout = ackley(x)
+%ACKLEY Ackley's function
 
 if nargin == 0
     varargout{1} = [-5,-5];     % LB
     varargout{2} = [5,5];       % UB
-    varargout{3} = [0,20];    % Zlim
+    varargout{3} = [0,20];      % Zlim
     varargout{4} = [-26.5,60];  % view
-    varargout{5} = [1,1];  % xmin    
+    varargout{5} = [1,1];       % xmin
     return;
 end
     varargout{1} = -20 * exp(-0.2 * sqrt( sum(x .* x, 2)/ 2)) ...
